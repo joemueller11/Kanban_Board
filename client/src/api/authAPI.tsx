a8f90ddf3f468d89f6a0c77bd218e594d0475d88
Joseph Mueller <joseph.m.email@gmail.com>
2025-03-29 13:05:37 -0400

    updated POST request to login route, auth updated in client utils to return values

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,6 +2,26 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try {
+    const response = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+      credentials: 'include'
+    });
+
+    if (!response.ok) {
+      throw new Error(`Login failed: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Login error:', error);
+    throw error;
+  }
 }
 
 
--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,50 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    if (!token) return null;
+    return jwtDecode<JwtPayload>(token);
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+    
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+try {
+  const decoded = jwtDecode<JwtPayload>(token);
+  if (!decoded.exp) return true;
+  
+  // Compare expiration time with current time
+  return decoded.exp * 1000 < Date.now();
+} catch (err) {
+  // If token can't be decoded, consider it expired
+  return true;
+}
   }
 
   getToken(): string {
     // TODO: return the token
+    return localStorage.getItem('id_token') || '';
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
-    // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+     // TODO: redirect to the home page
+    window.location.assign('/');
+
   }
 
   logout() {
     // TODO: remove the token from localStorage
-    // TODO: redirect to the login page
+      localStorage.removeItem('id_token');
+      // TODO: redirect to the login page
+      window.location.assign('/login');
   }
 }
 
